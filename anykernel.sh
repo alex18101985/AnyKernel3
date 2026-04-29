10ba6a12664f1ba1512547871933b9ad817fc43f
alex18101985 <alex18101985@gmail.com>
2026-04-29 15:06:49 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=AOSPA Marble GKI (KernelSU + SuSFS)
+kernel.string=AOSPA Marble GKI Kernel (KernelSU + SuSFS)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
